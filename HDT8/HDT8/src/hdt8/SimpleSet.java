fc580fa2675e2d2b6e0d6e60b8bbed09aee48ab9
jho1213gt <jho1213gt@gmail.com>
2014-10-04 13:37:52 -0600

    Ver 1.0.0

---
+++ HDT8/HDT8/src/hdt8/SimpleSet.java
@@ -0,0 +1,36 @@
+
+/*
+* Universidad del Valle de Guatemala
+* Algoritmos y Estructuras de Datos
+* Ing. DOuglas Barrios
+* Miembros:
+* - Max de Leon 13012
+* - Javier Bucaro 13033
+* - Kuk Ho Chung 13279
+*/
+
+package hdt8;
+
+import java.util.ArrayList;
+
+public class SimpleSet implements WordSet
+{
+	private ArrayList<Word> base;
+	
+	public SimpleSet()
+	{
+		base = new ArrayList<Word>();
+	}
+	
+	public Word get(Word word)
+	{
+		int index = base.indexOf(word);
+		if(index == -1) return null;
+		return base.get(index);
+	}
+	
+	public void add(Word wordObject)
+	{
+		base.add(wordObject);
+	}
+}
