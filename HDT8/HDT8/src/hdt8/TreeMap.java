6279e21a1878894d7af73fad40334f2a95a2e428
jho1213gt <jho1213gt@gmail.com>
2014-10-08 20:12:54 -0600

    Ver 1.0.5.1 (Ran)

--- HDT8/HDT8/src/hdt8/TreeMap.java
+++ HDT8/HDT8/src/hdt8/TreeMap.java
@@ -1,11 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package hdt8;
 /**
- *
+ * www.docjar.com/html/api/java/util/TreeMap.java.html
  * @author Javier Bucaro
  */
 public class TreeMap implements WordSet {
