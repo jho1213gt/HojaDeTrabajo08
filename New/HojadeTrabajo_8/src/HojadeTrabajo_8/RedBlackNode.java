6c4543e278e559f72632b0b60280b000e1c8c435
javobu94 <javobu_94@hotmail.com>
2014-10-10 07:52:47 -0600

    Ver (Pre-Final)

---
+++ New/HojadeTrabajo_8/src/HojadeTrabajo_8/RedBlackNode.java
@@ -0,0 +1,34 @@
+package HojadeTrabajo_8;
+
+import javax.swing.tree.TreeNode;
+/*
+* Universidad del Valle de Guatemala
+* Algoritmos y Estructuras de Datos
+* Ing. DOuglas Barrios
+* Miembros:
+* - Max de Leon 13012
+* - Javier Bucaro 13033
+* - Kuk Ho Chung 13279
+*/
+class RedBlackNode
+{
+        // Constructors
+    RedBlackNode( Comparable theElement )
+    {
+        this( theElement, null, null );
+    }
+
+    RedBlackNode( Comparable theElement, RedBlackNode lt, RedBlackNode rt )
+    {
+        element  = theElement;
+        left     = lt;
+        right    = rt;
+        color    = RedBlackTree.BLACK;
+    }
+
+        // Friendly data; accessible by other package routines
+    Comparable   element;    // The data in the node
+    RedBlackNode left;       // Left child
+    RedBlackNode right;      // Right child
+    int          color;      // Color
+}
---
+++ New/HojadeTrabajo_8/src/HojadeTrabajo_8/Word.java
@@ -0,0 +1,57 @@
+package HojadeTrabajo_8;
+/*
+* Universidad del Valle de Guatemala
+* Algoritmos y Estructuras de Datos
+* Ing. DOuglas Barrios
+* Miembros:
+* - Max de Leon 13012
+* - Javier Bucaro 13033
+* - Kuk Ho Chung 13279
+*/
+class Word implements Comparable<Word> {
+	private String word;
+	private String type;
+	
+	// Constructor, inicializa la palabra con su tipo
+	public Word(String word, String type)
+	{
+		this.word=word;
+		this.type=type;
+	}
+	
+	public Word()
+	{
+		this.word= "";
+		this.type="";
+	}
+	
+	// Comparadores, solo importa comparar la palabra, no el tipo.
+	public int compareTo(Word o)
+	{
+		return this.word.compareTo(o.getWord());
+	}
+	
+	public boolean equals(Object obj)
+	{
+		return (obj instanceof Word && getWord().equals(((Word)obj).getWord()));
+	}
+	
+	// Métodos de acceso..
+	public void setWord(String word)
+	{
+		this.word=word;
+	}
+	public void setType(String type)
+	{
+		this.type=type;
+	}
+	public String getWord()
+	{
+		return word;
+	}
+	
+	public String getType()
+	{
+		return type;
+	}
+}
