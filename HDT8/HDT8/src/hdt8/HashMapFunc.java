380fb399ec857f36272ae4d2f9562a30ad360b0b
Max <maxdlr.7@gmail.com>
2014-10-10 18:49:17 -0600

    Revisión Final con comentarios

--- HDT8/HDT8/src/hdt8/HashMapFunc.java
+++ HDT8/HDT8/src/hdt8/HashMapFunc.java
@@ -1,4 +1,3 @@
-
 package hdt8;
 
 /*
@@ -11,23 +10,22 @@ package hdt8;
 * - Kuk Ho Chung 13279
 */
 import java.util.HashMap;
-
+// Se implementa nuestra interface 
 public class HashMapFunc implements WordSet{
-    
+// Creamos un objeto de tipo HashMap (importando JCF)
     private HashMap<String, String> Map;
-    
+// Constructor    
      public HashMapFunc(){
        Map = new HashMap();
     }
-    
+// Método para agregar un nuevo objeto de tipo Word
     public void add(Word wordObject) {
         Map.put(wordObject.getWord(),wordObject.getType() );
     }
-
+// Método para obtener los valores de un objeto determinado
     public Word get(Word word) {
         if (!(Map.containsKey(word.getWord())))
             return null;
         return new Word(word.getWord(), Map.get(word.getWord()));
     }    
-    
 }
