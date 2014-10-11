1f9bd71ded0a64319d18cb33ab6c8d75652ac777
Max <maxdlr.7@gmail.com>
2014-10-10 18:52:35 -0600

    Revisión Final con comentarios

--- HDT8/HDT8/src/hdt8/RedBlackCom.java
+++ HDT8/HDT8/src/hdt8/RedBlackCom.java
@@ -10,19 +10,22 @@ package hdt8;
 * - Javier Bucaro 13033
 * - Kuk Ho Chung 13279
 */
+// Se implementa nuestra interface
 public class RedBlackCom implements WordSet{
+// Se crea un objeto de tipo RedBlackTree    
     RedBlackTree arbol;
-    
+// Constructor
     public RedBlackCom(){
         arbol = new RedBlackTree(new Word());
     }
-    
+// Mëtodo para obtener un valor del RedBlackTree 
     public Word get(Word word){
         Word temp;
+// Se llama al método find de la clase RedBlackTree
         temp =(Word) arbol.find(word);
         return temp;
     }
-    
+// Método para obtener un valor determinado    
     public void add(Word word){
         arbol.insert(word);
     }
